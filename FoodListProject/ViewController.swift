07b2433d5ea24eef4ff5844a6750c45c2d17a148
Egor Rubtsov <rubtsove777@gmail.com>
2021-07-31 20:27:05 +0300

    Initial Commit

---
+++ FoodListProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodListProject
+//
+//  Created by Егор on 31.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
