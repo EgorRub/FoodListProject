de7d54fb801744e0b1456129415621d40e1faf35
Egor Rubtsov <rubtsove777@gmail.com>
2021-08-01 15:56:12 +0300

    TableViewCommit-BbugFix

---
+++ FoodListProject/RecipeListTableTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  RecipeListTableTableViewController.swift
+//  FoodListProject
+//
+//  Created by Kovalyov Dmitry on 01.08.2021.
+//
+
+import UIKit
+
+class RecipeListTableTableViewController: UITableViewController {
+
+    private var recipeArray = Recipe.getRecipes()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 95
+
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipeArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let recipe = recipeArray[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = recipe.title
+        content.image = UIImage(named: recipe.titleForImage)
+        content.imageProperties.cornerRadius = tableView.rowHeight
+        
+        cell.contentConfiguration = content
+
+        return cell
+    }
+
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
