5bbc5cf784227c9ea9bba2cfa4d60a1355a43421
Kovalyov Dmitriy <kovdmkonst@gmail.com>
2021-08-01 18:46:37 +1000

    newTableVC view

--- FoodListProject/RecipeListTableTableViewController.swift
+++ FoodListProject/RecipeListTableTableViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 class RecipeListTableTableViewController: UITableViewController {
 
     private var recipeArray = Recipe.getRecipes()
-//    let editButton = UITableView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.rowHeight = 50
-        navigationItem.rightBarButtonItem = editButtonItem
+        tableView.rowHeight = 95
 
     }
 
@@ -40,45 +39,10 @@ class RecipeListTableTableViewController: UITableViewController {
         return cell
     }
 
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
 }
